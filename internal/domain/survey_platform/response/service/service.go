3ff1420b906eb2fa6dd4e9379dabb93e594ed02d
lccheungperry <109510587+lccheungperry@users.noreply.github.com>
2025-05-03 00:16:54 +0800

    feat: add response handlers (#6)
    Co-authored-by: Perry Cheung <cyberlchung@gmail.com>

--- internal/domain/survey_platform/response/service/service.go
+++ internal/domain/survey_platform/response/service/service.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"time"
 
-	"github.com/lccheungperry/OSP_backend/internal/domain/survey_platform/response/command"
+	bus_command "github.com/lccheungperry/OSP_backend/internal/domain/survey_platform/bus/command"
+	bus_query "github.com/lccheungperry/OSP_backend/internal/domain/survey_platform/bus/query"
+	resp_command "github.com/lccheungperry/OSP_backend/internal/domain/survey_platform/response/command"
 	"github.com/lccheungperry/OSP_backend/internal/domain/survey_platform/response/model"
+	resp_query "github.com/lccheungperry/OSP_backend/internal/domain/survey_platform/response/query"
 	"github.com/lccheungperry/OSP_backend/internal/domain/survey_platform/response/repository"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type ResponseService struct {
@@ -18,33 +20,46 @@ func NewResponseService(repo repository.ResponseRepository) *ResponseService {
 	return &ResponseService{repo: repo}
 }
 
-func (s *ResponseService) SubmitResponse(ctx context.Context, cmd *command.SubmitResponseCommand) (*model.Response, error) {
-	answers := make([]model.Answer, len(cmd.Answers))
-	for i, cmdAnswer := range cmd.Answers {
-		answers[i] = model.Answer{
-			QuestionID: cmdAnswer.QuestionID,
-			Value:      cmdAnswer.Value,
+func (s *ResponseService) HandleCommand(ctx context.Context, cmd bus_command.Command) error {
+	switch c := cmd.(type) {
+	case *resp_command.SubmitResponseCommand:
+		answers := make([]model.Answer, len(c.Answers))
+		for i, cmdAnswer := range c.Answers {
+			answers[i] = model.Answer{
+				QuestionID: cmdAnswer.QuestionID,
+				Value:      cmdAnswer.Value,
+			}
 		}
-	}
 
-	response := &model.Response{
-		SurveyID:  cmd.SurveyID,
-		Answers:   answers,
-		CreatedAt: time.Now(),
-	}
+		response := &model.Response{
+			SurveyID:  c.SurveyID,
+			Answers:   answers,
+			CreatedAt: time.Now(),
+		}
 
-	if err := s.repo.Create(ctx, response); err != nil {
-		return nil, err
+		return s.repo.Create(ctx, response)
+	case *resp_command.DeleteResponseCommand:
+		return s.repo.Delete(ctx, c.ID)
+	default:
+		return nil
 	}
-
-	return response, nil
 }
 
-func (s *ResponseService) GetResponses(ctx context.Context, surveyID primitive.ObjectID, page, pageSize int64) ([]*model.Response, int64, error) {
-	skip := (page - 1) * pageSize
-	return s.repo.FindBySurveyID(ctx, surveyID, skip, pageSize)
-}
-
-func (s *ResponseService) DeleteResponse(ctx context.Context, cmd *command.DeleteResponseCommand) error {
-	return s.repo.Delete(ctx, cmd.ID)
+func (s *ResponseService) HandleQuery(ctx context.Context, q bus_query.Query) (interface{}, error) {
+	switch query := q.(type) {
+	case *resp_query.GetResponsesQuery:
+		responses, total, err := s.repo.FindBySurveyID(ctx, query.SurveyID, query.Skip, query.Limit)
+		if err != nil {
+			return nil, err
+		}
+		return struct {
+			Responses []*model.Response `json:"responses"`
+			Total     int64             `json:"total"`
+		}{
+			Responses: responses,
+			Total:     total,
+		}, nil
+	default:
+		return nil, nil
+	}
 }
