f2dfdaad93159ec0c1f5fdac67fc9a80ac04db09
Perry Cheung <cyberlchung@gmail.com>
2025-05-02 22:15:25 +0800

    chore: formatting

--- internal/domain/survey_platform/question/repository/question.go
+++ internal/domain/survey_platform/question/repository/question.go
@@ -7,9 +7,7 @@ import (
 	"github.com/lccheungperry/OSP_backend/internal/domain/survey_platform/question/model"
 )
 
-var (
-	ErrQuestionNotFound = errors.New("question not found")
-)
+var ErrQuestionNotFound = errors.New("question not found")
 
 type QuestionRepository interface {
 	Create(ctx context.Context, question *model.Question) error
